ae85b8bd0ad256bb8d23bfe8fbbd8a7f1dc2ae18
AriesRedWar <philipjbowman5@gmail.com>
2022-12-11 13:24:53 -0500

    day 6 stuff updated

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\philip.DESKTOP-FPDCMF9\Documents\NC-State-Projects\ppp Python\Day 5\PY-PetFax
+set VIRTUAL_ENV=C:\Users\philip.DESKTOP-FPDCMF9\Documents\NC-State-Projects\ppp Python\Day 6\PY-PetFax
 
 if not defined PROMPT set PROMPT=$P$G
 
